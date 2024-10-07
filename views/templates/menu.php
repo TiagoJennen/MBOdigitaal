60ffcc3eba0e2b34f450664cbc3f04f0359ffaa3
TiagoJennen <514011@vistacollege.nl>
2024-10-07 10:40:24 +0200

    aa

--- views/templates/menu.php
+++ views/templates/menu.php
@@ -23,7 +23,7 @@
                 </li>
                 <li
                     class="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-blue-100 hover:text-blue-500 duration-200 cursor-pointer">
-                    <a href="/project/docent/index.php">Docent</a>
+                    <a href="/project/index.php">Docent</a>
                 </li>
             </ul>
         </nav>
